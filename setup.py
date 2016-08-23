43b1f38e48d720c1de45257d4c8a82cbb5cca62e
Julia Rizza <contato@juliarizza.com>
2016-08-23 18:37:47 -0300

    updating setup file

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-﻿from cx_Freeze import setup, Executable
+import sys
+from cx_Freeze import setup, Executable
 
-setup(
-    name = "Certifica!",
-    version = "1.0.0",
-    options = {"build_exe": {
-        "packages":["PyPDF2","pdfkit","email"],
-        "include_files": ["images/background.png",
-                          "images/favicon.png",
-                          "images/favicon.ico",
-                          "certificate_back.html",
-                          "certificate_front.html",
-                          "certificate_resp.html"
-                         ],
-        "include_msvcr": True,
-    }},
-    executables = [Executable("app.py",
-                            base="Win32GUI",
-			    icon="images/favicon.ico",
-			    shortcutName="Certifica!",
-			    shortcutDir="DesktopFolder",
-                  )]
-    )
+build_exe_options = {"packages": ["PyPDF2", "pdfkit", "email"],
+                     "excludes": ["tkinter"],
+                     "include_files": ["images/background.png",
+                                       "images/favicon.png",
+                                       "images/favicon.ico",
+                                       "certificate_back.html",
+                                       "certificate_front.html",
+                                       "certificate_resp.html"
+                                      ],
+#                     "include_msvcr": True,
+                    }
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Certifica!",
+        version = "1.0.0",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("app.py",
+                                  base=base,
+			                      icon="images/favicon.ico",
+                                  shortcutName="Certifica!",
+			                      shortcutDir="DesktopFolder",)])
